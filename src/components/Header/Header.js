f97592d5efc36d782b786d350885ff93693ea1af
Gabriel Garcia <gabrielgsdev@gmail.com>
2020-11-10 21:10:28 -0600

    Add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header () {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        <span>Post Management</span>
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
